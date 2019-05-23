4a9aa8ae5cf53075fc37f5f2216dd5c0a497b8ab
Arlingo Neto <arlindosilvaneto@gmail.com>
2019-05-23 13:59:23 -0300

    Application boilerplate with styleguide and material-ui

---
+++ src/config/app.js
@@ -0,0 +1,3 @@
+const config = {
+    token: 'BQBGIfmZGPwjjwpDw9DJhUfYx7u6W3MG2OFJoZCYxaojWD_clTtDkeXW7Db7iF83RJYcKzXdNfDzYMTu52eSr-O5hRvqzwOkOFpjKZjqh2lDFjmw31h7BOm-wmKTX7iedJ-4V9PqoOwqtxSBKp4wbvhvow'
+}
